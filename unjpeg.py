b719b1376ac34a5992d815eb4a78b79276830587
Alexander Vaagan <alexander.vaagan@inmeta.no>
2018-09-14 12:45:14 +0200

    Unet

--- unjpeg.py
+++ unjpeg.py
@@ -1,6 +1,8 @@
 from tensorflow.python.keras import Input, Model, optimizers
-from tensorflow.python.keras.callbacks import ModelCheckpoint
+from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.python.keras.layers import Convolution2D, MaxPooling2D, Conv2D, Add, Dropout, concatenate, UpSampling2D
+
+from progress_monitor import ProgressMonitor
 from tensorboard import TensorBoard
 
 
@@ -46,7 +48,7 @@ tb_gen = datagen.flow_from_directory(
         shuffle='input')
 
 
-def unet(pretrained_weights=None, input_size=(256, 256, 1)):
+def unet(pretrained_weights=None, input_size=(224, 224, 3)):
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
@@ -67,28 +69,28 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1)):
     drop5 = Dropout(0.5)(conv5)
 
     up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
-    merge6 = concatenate([drop4, up6], concat_axis=3)
+    merge6 = concatenate([drop4, up6], axis=3)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
     up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
-    merge7 = concatenate([conv3, up7], concat_axis=3)
+    merge7 = concatenate([conv3, up7], axis=3)
     conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
     conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 
     up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
-    merge8 = concatenate([conv2, up8], concat_axis=3)
+    merge8 = concatenate([conv2, up8], axis=3)
     conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
     conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
     up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
-    merge9 = concatenate([conv1, up9], concat_axis=3)
+    merge9 = concatenate([conv1, up9], axis=3)
     conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+    conv9 = Conv2D(3, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    # conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
 
-    model = Model(input=inputs, output=conv10)
+    model = Model(inputs=inputs, outputs=conv9)
 
     model.compile(optimizer=optimizers.Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
 
@@ -135,7 +137,8 @@ def DeepDenoiseSR2(image_dim):
 
 
 image_dim = [img_width, img_height, 3]
-model = DeepDenoiseSR2(image_dim)
+# model = DeepDenoiseSR2(image_dim)
+model = unet(None, input_size=image_dim)
 # model.summary()
 
 tb = TensorBoard(log_dir='logs/unjpeg', histogram_freq=0, write_graph=True, write_images=True,
@@ -143,6 +146,9 @@ tb = TensorBoard(log_dir='logs/unjpeg', histogram_freq=0, write_graph=True, writ
 checkpoint = ModelCheckpoint('model/unjpeg/model-{epoch:02d}-{val_loss:.6f}.hdf5', monitor='val_loss',
                              save_best_only=True,
                              mode='min', save_weights_only=True, verbose=1)
-callbacks_list = [checkpoint, tb]
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                              patience=4, min_lr=0.00001)
+img_export = ProgressMonitor(generator=tb_gen, path='logs/unjpeg')
+callbacks_list = [checkpoint, tb, reduce_lr, img_export]
 
 model.fit_generator(train_gen, validation_data=val_gen, steps_per_epoch=200, epochs=100, validation_steps=20, callbacks=callbacks_list)
